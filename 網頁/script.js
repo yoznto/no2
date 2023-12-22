0783db70d58a561c68c3598794aa6fab5765793c
yoznto <154240067+yoznto@users.noreply.github.com>
2023-12-22 23:03:07 +0800

    Add files via upload

---
+++ 網頁/script.js
@@ -0,0 +1,19 @@
+const lists = Document.querySelectorAll(".list");
+lists.forEach(function(item)
+{
+    item.addEventListener("click",function()
+    {
+        for(i =0 ;i<lists.length;i++)
+        {
+            lists[i].classList.remove("active");
+        }
+        item.classList.add("active");
+    })
+});
+const button = document.querySelector(".togglebtn");
+const navbar = document.querySelector(".navbar");
+button.addEventListener("click",function()
+{
+    button.classList.toggle("active");
+    navbar.classList.toggle("active");
+});
